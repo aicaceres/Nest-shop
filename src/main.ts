21ce92bebebec56692bf934fef46d026f0d7ceab
Alejandra Cáceres <alejandraicaceres@gmail.com>
2022-12-08 18:21:59 -0300

    Feature: upload and show product image

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const logger = new Logger('Main');
   app.setGlobalPrefix('api');
   app.useGlobalPipes(
     new ValidationPipe({
@@ -12,6 +13,6 @@ async function bootstrap() {
     }),
   );
   await app.listen(process.env.PORT);
-  console.log(`App running on port ${process.env.PORT}`);
+  logger.log(`App running on port ${process.env.PORT}`);
 }
 bootstrap();
