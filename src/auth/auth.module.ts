a8a0069f2dd9b488e14d78dc25df595de05d4ed0
Alejandra Cáceres <alejandraicaceres@gmail.com>
2022-12-10 08:53:49 -0300

    Feature: add jwt strategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,13 +1,43 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { User } from './entities/user.entity';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
-  imports: [TypeOrmModule.forFeature([User])],
-  exports: [TypeOrmModule],
+  providers: [AuthService, JwtStrategy],
+  imports: [
+    ConfigModule,
+    TypeOrmModule.forFeature([User]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        // use process.env directly or configService to inject the enviroment variable
+        // const secret = process.env.JWT_SECRET;
+        const secret = configService.get('JWT_SECRET');
+        return {
+          secret: secret,
+          signOptions: {
+            expiresIn: '3h',
+          },
+        };
+      },
+    }),
+    // JwtModule.register({
+    //   secret: process.env.JWT_SECRET,
+    //   signOptions: {
+    //     expiresIn: '3h',
+    //   },
+    // }),
+  ],
+  exports: [TypeOrmModule, JwtStrategy, PassportModule, JwtModule],
 })
 export class AuthModule {}
